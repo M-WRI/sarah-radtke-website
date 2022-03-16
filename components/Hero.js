75484f6e085ca65369275e9b47eda20ec002f4a3
Moritz Wright <me@moritzwright.com>
2022-03-15 21:52:02 -0600

    hero section demo done

--- components/Hero.js
+++ components/Hero.js
@@ -1,10 +1,14 @@
 import styles from "../styles/Hero.module.css";
 
-const Hero = () => {
+const Hero = ({ img, text }) => {
   return (
     <div className={styles.container}>
       <div className={styles.imageContainer}></div>
-      <div className={styles.heroTextContainer}></div>
+      <div className={styles.heroTextContainer}>
+        <h1 className={styles.heroText}>
+          Privatpraxis für Physiotherapie, Manuelle Therapie und Akupunktur
+        </h1>
+      </div>
     </div>
   );
 };
