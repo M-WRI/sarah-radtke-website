1f95b0d6e3d5bda73f8d942f150daa0d99486ba7
Moritz Wright <me@moritzwright.com>
2022-05-11 00:29:45 +0200

    animation hero done

--- components/Hero.js
+++ components/Hero.js
@@ -1,10 +1,71 @@
+import { useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
 import styles from "../styles/Hero.module.css";
 
 const Hero = ({ img, text, title, site }) => {
+  const { ref, inView } = useInView();
+
+  const animation = {
+    imageContainer: {
+      width: "90vw",
+      transition: {
+        delay: 0.5,
+        ease: [0.7, 0.135, 0.235, 0.99],
+        duration: 2.4,
+      },
+    },
+    image: {
+      y: 0,
+      transition: {
+        delay: 0.5,
+        ease: [0.7, 0.135, 0.235, 0.99],
+        duration: 1.2,
+      },
+    },
+    textContainer: {
+      opacity: 1,
+      y: "0%",
+      transition: {
+        delay: 0.8,
+        ease: [0.7, 0.135, 0.235, 0.99],
+        duration: 1.2,
+      },
+    },
+  };
+
+  const imgConCtrl = useAnimation();
+  const imgCtrl = useAnimation();
+  const textBox = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      imgConCtrl.start("imageContainer");
+      imgCtrl.start("image");
+      textBox.start("textContainer");
+    }
+  }, [inView, imgCtrl, imgConCtrl, textBox]);
+
   return (
-    <div className={styles.container}>
-      <div className={styles.imageContainer}></div>
-      <div
+    <div ref={ref} className={styles.container}>
+      <motion.div
+        initial={{ width: "100vw" }}
+        variants={animation}
+        animate={imgConCtrl}
+        className={styles.imageContainer}
+      >
+        <motion.div
+          initial={{ y: "100%" }}
+          variants={animation}
+          animate={imgCtrl}
+          className={styles.image}
+        ></motion.div>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: "50%" }}
+        variants={animation}
+        animate={textBox}
         className={
           site === "index"
             ? styles.heroTextContainerIndex
@@ -21,7 +82,7 @@ const Hero = ({ img, text, title, site }) => {
           </h1>
         )}
         {text && <p className={styles.heroText}>{text}</p>}
-      </div>
+      </motion.div>
     </div>
   );
 };
